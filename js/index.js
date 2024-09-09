c51bf11188bfa329eb06d7e6eb82fb198ecafc35
박진우 <somes228@gmail.com>
2024-09-09 17:58:19 +0900

    feat: 섹션1 이벤트 및 애니메이션까지 구현

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
 "use strict";
 
+// 헤더 스크롤 내릴시 이벤트
+
+const header = document.querySelector("header");
+const headerLogoT = document.querySelector("header .logo h1");
+const headerLogoS = document.querySelector("header .logo svg");
+const sidebarBlank = document.querySelector(".sidebar .blank");
+
+document.addEventListener("scroll", function () {
+  if (scrollY == 0) {
+    header.style.height = "82px";
+    headerLogoT.style.display = "block";
+    headerLogoS.style.display = "none";
+    sidebarBlank.style.height = "82px";
+  } else {
+    header.style.height = "50px";
+    headerLogoT.style.display = "none";
+    headerLogoS.style.display = "block";
+    sidebarBlank.style.height = "50px";
+  }
+});
+
 // 섹션1
 
 gsap.to(".sec1 .bg_box .first", {
@@ -23,6 +44,7 @@ gsap.to(".sec1 .bg_box .first", {
   },
   "clip-path":
     "polygon(20% 0%, 0% 20%, 30% 50%, 0% 80%, 20% 100%, 50% 70%, 80% 100%, 100% 80%, 70% 50%, 100% 20%, 80% 0%, 50% 30%)",
+  "border-radius": "0%",
 });
 
 gsap.to(".sec1 .bg_box .third", {
